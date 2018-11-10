a6216f066f9628780acbfd4cb1a624073695984a
Anna Kaplan <annakaplanart@gmail.com>
2018-11-10 16:55:07 -0500

    added spheres to eyes, and updated changeX/Y positions

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import * as ml5 from 'ml5'
+import * as THREE from 'three'
+
 import loopThroughPoses from './threeJs/nose'
+
 let video = document.createElement('video')
-video.setAttribute('width', 225);
-video.setAttribute('height', 225);
-
+video.setAttribute('width', 255);
+video.setAttribute('height', 255);
 video.autoplay = true
-// video.hidden = true;
-// let canvas = document.createElement('canvas')
-// canvas.id = "myCanvas"
 document.body.appendChild(video)
-// document.body.appendChild(canvas)
-
-let obj = {}
 
 // get the users webcam stream to render in the video
 navigator.mediaDevices.getUserMedia({ video: true, audio: false })
@@ -24,23 +20,19 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     });
 
 let options = { 
-
   flipHorizontal: true,
   minConfidence: 0.5
- 
  } 
+
 let poseNet = ml5.poseNet(video, options, modelReady)
 
-
-
   //three.js code
-import * as THREE from 'three'
-
 const scene = new THREE.Scene();
-
+scene.background = new THREE.Color( 0xf0f0f0 );
 // Create a basic perspective camera
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, (window.innerWidth/2) /(window.innerHeight/2), 0.1, 1000 );
 camera.position.z = 20;
+
 
 // Create a renderer with Antialiasing
 const renderer = new THREE.WebGLRenderer({antialias:true});
@@ -49,69 +41,54 @@ const renderer = new THREE.WebGLRenderer({antialias:true});
 // renderer.setClearColor("#2E2B40");
 
 // Configure renderer size
-renderer.setSize( window.innerWidth / 2, window.innerHeight / 2 );
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
 
 // Append Renderer to DOM
 document.body.appendChild( renderer.domElement );
 
-
-// ------------------------------------------------
-// FUN STARTS HERE
-// ------------------------------------------------
+//raycaster assists in mouse picking
 
 let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-let material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
+let sphereGeo = new THREE.SphereGeometry(1, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+let material = new THREE.MeshPhongMaterial( { color: "#433F81" } );
 let cube01 = new THREE.Mesh( geometry, material );
-let cube02 = new THREE.Mesh( geometry, material );
-let cube03 = new THREE.Mesh( geometry, material );
-scene.add( cube01, cube02, cube03 );
+let cube02 = new THREE.Mesh( sphereGeo, material );
+let cube03 = new THREE.Mesh( sphereGeo, material );
+let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+			
+scene.add(ambientLight, cube01, cube02, cube03 );
 
 // Render Loop
 
 let lastXPosition = 100;
 let lastYPosition = 100;
-let changeX;
-let changeY;
+let changeX = 1;
+let changeY = 1;
 
 const changeYXPosition = (faceObj, shape, leftEyeShape, rightEyeShape) => {
-  if (!changeX){
-    changeX = 0
-    changeY = 0
-  }
+  console.log("Change", faceObj.x, lastXPosition)
+
   changeX = faceObj.x - lastXPosition
   changeY = faceObj.y - lastYPosition
   
   console.log('changes x,y', changeX, changeY)
-  shape.position.x += (changeX * 0.5)
-  shape.position.y += -(changeY * 0.3)
-  rightEyeShape.position.x = shape.position.x + 1
-  rightEyeShape.position.y = shape.position.y + 2
-  leftEyeShape.position.x = shape.position.x - 1
-  leftEyeShape.position.y = shape.position.y + 2
+  shape.position.x += (changeX * 0.12)
+  shape.position.y += -(changeY * 0.12)
+  rightEyeShape.position.x = shape.position.x + 3
+  rightEyeShape.position.y = shape.position.y + 4
+  leftEyeShape.position.x = shape.position.x - 3
+  leftEyeShape.position.y = shape.position.y + 4
 
   console.log(`shape position x, y`, shape.position.x, shape.position.y)
   lastXPosition = faceObj.x
   lastYPosition = faceObj.y
   console.log('lastpositions', lastXPosition, lastYPosition)
 }
-
+// import changeYXPosition from './threeJs/changeXY'
 const render = function (aNose, shape, aRightEye, aLeftEye) {
-    // let changeX;
-    // let changeY;
-    changeYXPosition(aNose, shape, aRightEye, aLeftEye)
-    // changeYXPosition(aRightEye, cube02, 0 , 0)
-    // changeYXPosition(aLeftEye, cube03, 0 , 0)
-  // console.log('last pos', lastPosition)
-  // if (!change){
-  //   change = 1
-  // }
-  // change = aNose.x - lastPosition
-  // console.log("change", change)
-  // cube01.position.x = cube01.position.x + (change * 1.1)
-  // cube01.position.y = 1;
-  // console.log("position",cube01.position.x)
-  // lastPosition = aNose.x
-  
+  changeYXPosition(aNose, shape, aRightEye, aLeftEye)
+  aRightEye.rotation.x +=0.1
+  aLeftEye.rotation.x -=0.1
   renderer.render(scene, camera);
 };
 
@@ -119,22 +96,17 @@ let nose = {}
 let rightEye = {}
 let leftEye = {}
 
-poseNet.on('pose', function(results) {
+poseNet.on('pose',  function(results) {
   let poses = results;
-  loopThroughPoses(poses, nose, rightEye, leftEye)
+ loopThroughPoses(poses, nose, rightEye, leftEye)
   let estimatedNose = {
     x: Math.round(nose.x),
     y: Math.round(nose.y)
   }
-  // let estimatedREye = {
-  //   x: Math.round(rightEye.x),
-  //   y: Math.round(rightEye.y)
-  // }
-  // let estimatedLEye = {
-  //   x: Math.round(leftEye.x),
-  //   y: Math.round(leftEye.y)
-  // }
+  if (estimatedNose.x && estimatedNose.y){
+  console.log("On POSE", estimatedNose.x)
   render(estimatedNose, cube01, cube02, cube03)
+  }
 });
 
 function modelReady() {
@@ -148,9 +120,9 @@ window.addEventListener( 'resize', onWindowResize, false );
 			
 function onWindowResize() {
 
-   camera.aspect = window.innerWidth / window.innerHeight;
+   camera.aspect = (window.innerWidth/2) / (window.innerHeight/2);
    camera.updateProjectionMatrix();
-   renderer.setSize( window.innerWidth, window.innerHeight );
-  //  video.setAttribute('width', window.innerWidth);
-  //  video.setAttribute('height', window.innerHeight);
+   renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+  //  video.setAttribute('width', window.innerWidth/2);
+  //  video.setAttribute('height', window.innerWidth/2);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    entry: './index.js', // assumes your entry point is the index.js in the root of your project folder
-    mode: 'development',
-    output: {
-      path: __dirname, // assumes your bundle.js will also be in the root of your project folder
-      filename: 'bundle.js'
-    },
-    devtool: 'source-maps',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          
-        }
-      ]
-    }
+  entry: './index.js', // assumes your entry point is the index.js in the root of your project folder
+  mode: 'development',
+  output: {
+    path: __dirname, // assumes your bundle.js will also be in the root of your project folder
+    filename: 'bundle.js'
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        
+      }
+    ]
   }
+}
